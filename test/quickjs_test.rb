c8fadfe35d011f42b2d79b5202b95a57c06a75de
hmsk <k.hamasaki@gmail.com>
2024-07-13 21:07:58 -0700

    reorganize test cases

--- test/quickjs_test.rb
+++ test/quickjs_test.rb
@@ -9,107 +9,106 @@ class QuickjsTest < Test::Unit::TestCase
     end
   end
 
-  test "throw an exception" do
+  def assert_code(code, expected)
+    assert_equal(::Quickjs.eval_code(code), expected)
+  end
+
+  class ResultConversion < QuickjsTest
+    test "null becomes nil" do
+      assert_code("null", nil)
+    end
+
+    test "undefined becomes a specific constant" do
+      assert_code("undefined", Quickjs::Value::UNDEFINED)
+      assert_code("const obj = {}; obj.missing;", Quickjs::Value::UNDEFINED)
+    end
+
+    test "NaN becomes a specific constant" do
+      assert_code("Number('whatever')", Quickjs::Value::NAN)
+    end
+
+    test "string becomes String" do
+      assert_code("'1'", "1")
+    end
+
+    test "number for integer becomes Integer" do
+      assert_code("2+3", 5)
+      assert_code("18014398509481982n", 18014398509481982)
+    end
+
+    test "number for float becomes Float" do
+      assert_code("1.0", 1.0)
+      assert_code("2 ** 0.5", 1.4142135623730951)
+    end
+
+    test "boolean becomes TruClass/FalseClass" do
+      assert_code("false", false)
+      assert_code("true", true)
+      assert_code("1 === 1", true)
+      assert_code("1 == 3", false)
+    end
+
+    test "plain k-v object becomes Hash" do
+      assert_code("const obj = {}; obj", {})
+      assert_code("const obj = { a: '1', b: 1 }; obj;", { 'a' => '1', 'b' => 1 })
+    end
+
+    test "plain array object becomes Array" do
+      assert_code("[1, 2, 3]", [1, 2, 3])
+      assert_code("['a', 2, 'third']", ['a', 2, 'third'])
+      assert_code("[1, 2, { 'third': 'sad' }]", [1, 2, { 'third' => 'sad' }])
+    end
+  end
+
+  test "throws an exception transparently" do
     assert_raise_with_message(RuntimeError, /SyntaxError:/) { ::Quickjs.eval_code("}{") }
   end
 
-  test "support returning null" do
-    assert_equal(::Quickjs.eval_code("null"), nil)
-    assert_equal(::Quickjs.eval_code("const func = () => null; func();"), nil)
+  test "returns Promise (resolved) with awaiting result automatically" do
+    assert_code("const promise = new Promise((res) => { res('awaited yo') });promise", "awaited yo")
   end
 
-  test "support returning undefined" do
-    assert_equal(::Quickjs.eval_code("undefined"), Quickjs::Value::UNDEFINED)
-    assert_equal(::Quickjs.eval_code("const obj = {}; obj.key;"), Quickjs::Value::UNDEFINED)
-  end
-
-  test "support returning NaN" do
-    assert_equal(::Quickjs.eval_code("Number('whatever')"), Quickjs::Value::NAN)
-  end
-
-  test "support returning string" do
-    assert_equal(::Quickjs.eval_code("'1'"), "1")
-    assert_equal(::Quickjs.eval_code("const func = () => 'hello'; func();"), "hello")
-  end
-
-  test "support returning integer" do
-    assert_equal(::Quickjs.eval_code("2+3"), 5)
-    assert_equal(::Quickjs.eval_code("const func = () => 8; func();"), 8)
-    assert_equal(::Quickjs.eval_code("18014398509481982n"), 18014398509481982)
-  end
-
-  test "support returning float" do
-    assert_equal(::Quickjs.eval_code("1.0"), 1.0)
-    assert_equal(::Quickjs.eval_code("2 ** 0.5"), 1.4142135623730951)
-  end
-
-  test "support returning boolean" do
-    assert_equal(::Quickjs.eval_code("false"), false)
-    assert_equal(::Quickjs.eval_code("true"), true)
-    assert_equal(::Quickjs.eval_code("const func = () => 1 == 1; func();"), true)
-    assert_equal(::Quickjs.eval_code("const func = () => 1 == 3; func();"), false)
-  end
-
-  test "support returning plain object/array" do
-    assert_equal(::Quickjs.eval_code("const func = () => ({ a: '1', b: 1 }); func();"), { 'a' => '1', 'b' => 1 })
-    assert_equal(::Quickjs.eval_code("const func = () => ({ funcCantRemain: () => {} }); func();"), {})
-    assert_equal(::Quickjs.eval_code("[1,2,3]"), [1,2,3])
-  end
-
-  test "support returning Promise (resolved) with awaiting result automatically" do
-    assert_equal(::Quickjs.eval_code("const promise = new Promise((res) => { res('awaited yo') });promise"), "awaited yo")
-  end
-
-  test "support returning Promise (rejected) with awaiting result automatically" do
+  test "returns Promise (rejected) with awaiting result automatically" do
     assert_raise_with_message(RuntimeError, /asynchronously sad/) do
       ::Quickjs.eval_code("const promise = new Promise((res) => { throw 'asynchronously sad' });promise")
     end
   end
 
-  test "std is disabled" do
-    assert_equal(::Quickjs.eval_code("typeof std === 'undefined'"), true)
-  end
-
-  test "os is disabled" do
-    assert_equal(::Quickjs.eval_code("typeof os === 'undefined'"), true)
-  end
-
-  test "std can be enabled" do
+  test "std module can be enabled" do
+    assert_code("typeof std === 'undefined'", true)
     assert_equal(::Quickjs.eval_code("!!std.urlGet", { features: [::Quickjs::MODULE_STD] }), true)
   end
 
-  test "os can be enabled" do
+  test "os module can be enabled" do
+    assert_code("typeof os === 'undefined'", true)
     assert_equal(::Quickjs.eval_code("!!os.kill", { features: [::Quickjs::MODULE_OS] }), true)
   end
 
-  class QuickjsTestVm < Test::Unit::TestCase
-    class WithPlainVM < QuickjsTestVm
+  class QuickjsVmTest < Test::Unit::TestCase
+    class WithPlainVM < QuickjsVmTest
       setup { @vm = Quickjs::VM.new }
       teardown { @vm = nil }
 
-      test "VM maintains runtime and context" do
+      test "maintains the same context within a vm" do
         @vm.eval_code('const a = { b: "c" };')
         assert_equal(@vm.eval_code('a.b'), "c")
         @vm.eval_code('a.b = "d"')
         assert_equal(@vm.eval_code('a.b'), "d")
       end
 
-      test "VM doesn't eval codes anymore after disposing" do
+      test "raises if eval_code is called after disposing" do
         @vm.eval_code('const a = { b: "c" };')
         @vm.dispose!
         assert_raise_with_message(RuntimeError, /disposed/) { @vm.eval_code('a.b = "d"') }
       end
 
-      test "VM does not enable std features as default" do
+      test "does not enable std/os module as default" do
         assert_equal(@vm.eval_code("typeof std === 'undefined'"), true)
-      end
-
-      test "VM does not enable os features as default" do
         assert_equal(@vm.eval_code("typeof os === 'undefined'"), true)
       end
     end
 
-    test "VM accepts some options to constrain its resource" do
+    test "accepts some options to constrain its resource" do
       vm = Quickjs::VM.new(
         memory_limit: 1024 * 1024,
         max_stack_size: 1024 * 1024,
@@ -117,27 +116,27 @@ class QuickjsTest < Test::Unit::TestCase
       assert_equal(vm.eval_code('1+2'), 3)
     end
 
-    test "VM enables std feature" do
+    test "enables std module via features option" do
       vm = Quickjs::VM.new(
         features: [::Quickjs::MODULE_STD],
       )
       assert_equal(vm.eval_code("!!std.urlGet"), true)
     end
 
-    test "VM enables os feature" do
+    test "enables os module via features option" do
       vm = Quickjs::VM.new(
         features: [::Quickjs::MODULE_OS],
       )
       assert_equal(vm.eval_code("!!os.kill"), true)
     end
 
-    test "gets timeout" do
+    test "gets timeout from evaluation" do
       vm = Quickjs::VM.new
 
       assert_raise_with_message(RuntimeError, /interrupted/) { vm.eval_code("while(1) {}") }
     end
 
-    test "VM accepts timeout_msec to control maximum evaluation time" do
+    test "accepts timeout_msec option to control maximum evaluation time" do
       vm = Quickjs::VM.new(timeout_msec: 200)
 
       started = Time.now.to_f * 1000
@@ -145,90 +144,60 @@ class QuickjsTest < Test::Unit::TestCase
       assert_in_delta(started + 200, Time.now.to_f * 1000, 5) # within 5 msec
     end
 
-    class GlobalFunction < QuickjsTestVm
+    class GlobalFunction < QuickjsVmTest
       setup { @vm = Quickjs::VM.new }
-      teardown { @vm = nil }
+      teardown { @vm.dispose!; @vm = nil }
 
-      test "define global function that accepts no args" do
-        @vm.define_function("callRuby") do
-          ['Message', 'from', 'Ruby'].join(' ')
+      [
+        {
+          subject: "accepts a block with blank args",
+          js: "callRuby()",
+          defined_function: Proc.new { ['Message', 'from', 'Ruby'].join(' ') },
+          result: 'Message from Ruby',
+        },
+        {
+          subject: 'accepts a block with an arg',
+          js: "greetingTo('Rick')",
+          defined_function: Proc.new { |arg1| ['Hello!', arg1].join(' ') },
+          result: 'Hello! Rick',
+        },
+        {
+          subject: 'accepts a block with two args',
+          js: "concat('Ri', 'ck')",
+          defined_function: Proc.new { |arg1, arg2| "#{arg1}#{arg2}" },
+          result: 'Rick',
+        },
+        {
+          subject: 'accepts a block with many args',
+          js: "buildCSV('R', 'i', 'c', 'k')",
+          defined_function: Proc.new { |arg1, arg2, arg3, arg4| [arg1, arg2, arg3, arg4].join(' ') },
+          result: 'R i c k',
+        },
+        {
+          subject: 'accepts a block with many args including an optional one',
+          js: "callName('R', 'i', 'c', 'k')",
+          defined_function: Proc.new { |arg1, arg2, arg3, arg4, arg5 = 'Song'| [arg1, arg2, arg3, arg4].join('') + " #{arg5}" },
+          result: 'Rick Song',
+        },
+      ].each do |test_case, i|
+        test "define_function #{test_case[:subject]}" do
+          @vm.define_function(test_case[:js].scan(/(.+)\(.+$/).first.first, &test_case[:defined_function])
+          assert_equal(@vm.eval_code(test_case[:js]), test_case[:result])
         end
-
-        assert_equal(@vm.eval_code("callRuby()"), 'Message from Ruby')
       end
 
-      test "define global function that accepts an arg" do
-        @vm.define_function("greetingTo") do |arg1|
-          ['Hello!', arg1].join(' ')
+      [
+        ["'symsym'", :symsym],
+        ["null", nil],
+        ["3", 3],
+        ["3.14", 3.14],
+        ["true", true],
+        ["false", false],
+      ].each do |js, ruby|
+        test "retuned {ruby} by Ruby is #{js} in VM" do
+          @vm.define_function("get_ret") { ruby }
+          assert_equal(@vm.eval_code("get_ret() === #{js}"), true)
         end
-
-        assert_equal(@vm.eval_code("greetingTo('Rick')"), 'Hello! Rick')
-      end
-
-      test "define global function that accepts two args" do
-        @vm.define_function("concat") do |arg1, arg2|
-          "#{arg1}#{arg2}"
-        end
-
-        assert_equal(@vm.eval_code("concat('Ri', 'ck')"), 'Rick')
-      end
-
-      test "define global function that accepts many args" do
-        @vm.define_function("buildCSV") do |arg1, arg2, arg3, arg4|
-          [arg1, arg2, arg3, arg4].join(' ')
-        end
-
-        assert_equal(@vm.eval_code("buildCSV('R', 'i', 'c', 'k')"), 'R i c k')
-      end
-
-      test "define global function that accepts many args including an optional one" do
-        @vm.define_function("callName") do |arg1, arg2, arg3, arg4, arg5 = 'Song'|
-          [arg1, arg2, arg3, arg4].join('') + ' ' + arg5
-        end
-
-        assert_equal(@vm.eval_code("callName('R', 'i', 'c', 'k')"), 'Rick Song')
-      end
-
-      test "global timeout still work" do
-        pend 'apply timeout for evaluating proc'
-        @vm.define_function("infinite") { loop {} }
-        assert_raise_with_message(RuntimeError, /interrupted/) { @vm.eval_code("infinite();") }
-      end
-
-      test "returns symbol as string" do
-        @vm.define_function("get_sym") { :symsym }
-
-        assert_equal(@vm.eval_code("get_sym() === 'symsym'"), true)
-      end
-
-      test "returns nil as null" do
-        @vm.define_function("get_nil") { nil }
-
-        assert_equal(@vm.eval_code("get_nil() === null"), true)
-      end
-
-      test "returns fixnum as number" do
-        @vm.define_function("get_fixnum") { 3 }
-
-        assert_equal(@vm.eval_code("get_fixnum() === 3"), true)
-      end
-
-      test "returns float as number" do
-        @vm.define_function("get_float") { 3.14 }
-
-        assert_equal(@vm.eval_code("get_float() === 3.14"), true)
-      end
-
-      test "returns true as is" do
-        @vm.define_function("get_true") { true }
-
-        assert_equal(@vm.eval_code("get_true() === true"), true)
-      end
-
-      test "returns false as is" do
-        @vm.define_function("get_false") { false }
-
-        assert_equal(@vm.eval_code("get_false() === false"), true)
       end
 
       test "returns array as is if serializable" do
@@ -247,6 +216,12 @@ class QuickjsTest < Test::Unit::TestCase
         @vm.define_function("get_class") { Class.new }
 
         assert_match(/#<Class:/, @vm.eval_code("get_class()"))
+      end
+
+      test "global timeout still works" do
+        pend 'apply timeout for evaluating proc'
+        @vm.define_function("infinite") { loop {} }
+        assert_raise_with_message(RuntimeError, /interrupted/) { @vm.eval_code("infinite();") }
       end
     end
   end
