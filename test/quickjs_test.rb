3b172fd05f89d072c0e9c800e499bf57a01adfe3
hmsk <k.hamasaki@gmail.com>
2025-03-18 07:56:57 -0700

    ensure awaiting quickjs' timer messes other threads

--- test/quickjs_test.rb
+++ test/quickjs_test.rb
@@ -581,5 +581,66 @@ class QuickjsTest < Test::Unit::TestCase
         assert_equal('    at <eval> (<code>)', trace[3])
       end
     end
+
+    class ProcessBlocking < QuickjsVmTest
+      setup do
+        @vm = Quickjs::VM.new(timeout_msec: 1200, features: [::Quickjs::MODULE_OS])
+      end
+      teardown { @vm = nil }
+
+      def run_threads(&block)
+        result = []
+        t1 = Thread.new { 5.times { |i| result << 't1'; sleep 0.1 } }
+        t2 = Thread.new { block.call; result << 't2' }
+        [t1, t2].each { |t| t.join }
+        result
+      end
+
+      def assert_sleep_a_sec_within_thread(&block)
+        assert_equal(run_threads(&block), %w(t1 t1 t1 t1 t1 t2))
+      end
+
+      def refute_sleep_a_sec_within_thread(&block)
+        refute_equal(run_threads(&block), %w(t1 t1 t1 t1 t1 t2))
+      end
+
+      test 'ensure Kernel#sleep is fine' do
+        assert_sleep_a_sec_within_thread do
+          sleep 1
+        end
+      end
+
+      test 'ensure Kernel#sleep via a provided function is fine' do
+        @vm.define_function 'rbsleep' do |n|
+          sleep n
+        end
+
+        assert_sleep_a_sec_within_thread do
+          @vm.eval_code('await rbsleep(1);')
+        end
+
+        assert_sleep_a_sec_within_thread do
+          @vm.eval_code('async function top () { await new Promise(async resolve => { rbsleep(1); resolve(); }); } await top();')
+        end
+      end
+
+      test 'awaiting os.setTimeout messes' do
+        refute_sleep_a_sec_within_thread do
+          @vm.eval_code('await new Promise(resolve => os.setTimeout(resolve, 1000));')
+        end
+      end
+
+      test 'awaiting async function which wraps os.setTimeout messes' do
+        refute_sleep_a_sec_within_thread do
+          @vm.eval_code('async function top () { await new Promise(resolve => os.setTimeout(resolve, 1000)); } await top();')
+        end
+      end
+
+      test 'awaiting os.sleepAsync messes' do
+        refute_sleep_a_sec_within_thread do
+          @vm.eval_code('async function top () { await os.sleepAsync(1000); } await top();');
+        end
+      end
+    end
   end
 end
